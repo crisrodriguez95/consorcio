cc6c58ef1ca6c24c22dd67dbbc377db209fc0d6a
danylove95 <danylove9569@hotmail.com>
2021-06-09 17:52:54 -0500

    inicio

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsuarioController extends AbstractController
+{
+    /**
+     * @Route("/usuario", name="usuario")
+     */
+    public function index(): Response
+    {
+        return $this->render('assets/index.html.twig', [
+            'controller_name' => 'UsuarioController',
+        ]);
+    }
+}
